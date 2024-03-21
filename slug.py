4d4f65d5139ee5ed0b2d29b59e248ea04ac4fbe2
ChiPhuongHuynh <chi.phuong.ngoc.huynh@vanderbilt.edu>
2024-03-20 23:41:32 -0500

    links extracted

--- slug.py
+++ slug.py
@@ -22,3 +22,20 @@ datapoints = cur_string.split("},")
 eval = str(body.find("script", id="evaluation-table-data"))
 
 f.write(eval)
+
+
+eval = datatable[(datatable.find("{")) + 1:]
+sample = eval.split("{\"table_id\"")
+rows = []
+for s in sample:
+    s = s.split("\"tags\"")
+    data = s[0].split(", \"")
+    tag = s[1].split("}, {")
+    name = data[3].split(": ")[1][1:-1]
+    params = data[18].split(": ")[1]
+    accuracy = (data[17].split(": ")[2])
+    hardware_req = (data[19].split(": ")[1])
+    tags = []
+    for i in tag:
+        tags.append(i.split(", ")[1].split(": ")[1][1:-1])
+    rows.append([name, accuracy, params, hardware_req, tags])
