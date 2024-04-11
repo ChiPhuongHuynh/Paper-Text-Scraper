42e33baec0f273c875c516103767415e2cdd0a38
ChiPhuongHuynh <chi.phuong.ngoc.huynh@vanderbilt.edu>
2024-04-10 20:05:50 -0500

    0.05 neural network

--- dataprocess.py
+++ dataprocess.py
@@ -8,10 +8,9 @@ df = pd.read_csv("complete.csv", delimiter=",", index_col=False)
 merged_df = pd.merge(df, df1[['Task', 'Dataset', 'difficulty']], on=['Task', 'Dataset'], how='left')
 #print(merged_df)
 X_categorical = pd.get_dummies(merged_df[['Dataset', 'Task']], drop_first=True)
-X_numerical = merged_df[['accuracy', 'accuracy_norm', 'difficulty']]
+X_numerical = merged_df[['accuracy_norm', 'difficulty']]
 X_processed = pd.concat([X_categorical, X_numerical], axis=1)
 
 print(X_processed)
 
-#max_accuracy_df = df.groupby(['Dataset', 'Task'])['accuracy'].max().reset_index()
-#print(max_accuracy_df)
+X_processed.to_csv("dataprocessed.csv", index=False)
--- neuralnetwork.py
+++ neuralnetwork.py
@@ -4,3 +4,34 @@ import tensorflow as tf
 from tensorflow import keras
 from sklearn.model_selection import train_test_split
 
+df = pd.read_csv("dataprocessed.csv", index_col=False)
+df.dropna(inplace=True)
+bool_cols = ['Dataset_CIFAR-100', 'Dataset_COCO minivar', 'Dataset_ImageNet', 'Dataset_ImageNet 256x256', 'Dataset_MNIST', 'Dataset_SVHN', 'Task_Image Generation', 'Task_Object Detection']
+df[bool_cols] = df[bool_cols].astype(int)
+#print(df)
+round_cols = ['accuracy_norm', 'difficulty']
+df[round_cols] = df[round_cols].round(2)
+print(df)
+
+y = df['difficulty']
+X = df.drop('difficulty', axis=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+#print(X_train.tail(X_train.shape[0]-1))
+print(X_train.shape[1])
+
+model = keras.Sequential([
+    keras.layers.Dense(1, activation='relu', input_shape=(X_train.shape[1],)),
+    #keras.layers.Dense(1, activation='relu'),
+    keras.layers.Dense(1)
+])
+opt = keras.optimizers.Adam(learning_rate=0.0001)
+model.compile(optimizer=opt, loss='mean_squared_error')
+
+# Train the model
+model.fit(X_train, y_train, epochs=50, batch_size=32, validation_split=0.1)
+
+# Evaluate the model on the test set
+loss= model.evaluate(X_test, y_test)
+print('Test loss:', loss)
+predictions = model.predict(X_test)
+print(predictions)
